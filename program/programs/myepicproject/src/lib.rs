b50b7686ea3c7bed36b7253fe87914cd239b0b72
brown <brown2243@naver.com>
2022-10-16 21:24:43 +0900

    chore: project

---
+++ program/programs/myepicproject/src/lib.rs
@@ -0,0 +1,96 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Ev3ZHURzcwyzi7RZ2xeULVdm7TQsKEw1gpGRC3gevSoB");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+            like_count: 0,
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs = base_account.gif_list.len() as u64;
+        Ok(())
+    }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn like_gif(ctx: Context<LikeGif>, gif_link: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let gif_list = base_account
+            .gif_list
+            .clone()
+            .iter()
+            .map(|v| {
+                let mut count = v.like_count;
+                if v.gif_link == gif_link {
+                    count += 1;
+                }
+                let t = ItemStruct {
+                    gif_link: v.gif_link.to_string(),
+                    user_address: v.user_address,
+                    like_count: count,
+                };
+                t
+            })
+            .collect::<Vec<ItemStruct>>();
+
+        base_account.gif_list = gif_list;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+// increase like
+#[derive(Accounts)]
+pub struct LikeGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub like_count: u64,
+}
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
